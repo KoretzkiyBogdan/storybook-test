3b20a8455354b76562082186163b1732c84493c9
Bohdan Koretskyi <bkoretskyi@bcdtriptech.com>
2020-10-22 14:02:08 +0300

    new version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+  refs: {
+    "chromatic-published-Storybook": {
+      // The title of your Storybook
+      title: "Design System",
+      // The url provided by Chromatic when it was published
+      url: "https://master--5f90afa13959cf0022a4b3a2.chromatic.com",
+    },
+  },
+};
